fd4e41d8c35de1f4599e06ce4ffa98f9d4bc1dbf
Zimomo333 <lzh120908717@gmail.com>
2021-04-25 00:43:04 +0800

    table style

--- src/components/table.vue
+++ src/components/table.vue
@@ -4,6 +4,53 @@
     <ur-table-column prop="name" label="姓名" width="180"></ur-table-column>
     <ur-table-column prop="address" label="地址"></ur-table-column>
   </ur-table>
+  <h4>斑马纹表格</h4>
+  <ur-table :data="tableData" stripe style="width: 100%">
+    <ur-table-column prop="date" label="日期" width="180"></ur-table-column>
+    <ur-table-column prop="name" label="姓名" width="180"></ur-table-column>
+    <ur-table-column prop="address" label="地址"></ur-table-column>
+  </ur-table>
+  <h4>带边框表格</h4>
+  <ur-table :data="tableData" border style="width: 100%">
+    <ur-table-column prop="date" label="日期" width="180"></ur-table-column>
+    <ur-table-column prop="name" label="姓名" width="180"></ur-table-column>
+    <ur-table-column prop="address" label="地址"></ur-table-column>
+  </ur-table>
+  <h4>多选</h4>
+  <ur-table
+    ref="multipleTable"
+    :data="tableData"
+    tooltip-effect="dark"
+    style="width: 100%"
+    @selection-change="handleSelectionChange"
+  >
+    <ur-table-column type="selection" width="55"></ur-table-column>
+    <ur-table-column label="日期" width="120">
+      <template #default="scope">{{ scope.row.date }}</template>
+    </ur-table-column>
+    <ur-table-column prop="name" label="姓名" width="120"></ur-table-column>
+    <ur-table-column prop="address" label="地址" show-overflow-tooltip></ur-table-column>
+  </ur-table>
+  <h4>自定义操作</h4>
+  <ur-table :data="tableData" style="width: 100%">
+    <ur-table-column label="日期" width="180">
+      <template #default="scope">
+        <i class="ur-icon-time"></i>
+        <span style="margin-left: 10px">{{ scope.row.date }}</span>
+      </template>
+    </ur-table-column>
+    <ur-table-column label="姓名" width="180">
+      <template #default="scope">
+        <p>姓名: {{ scope.row.name }}</p>
+      </template>
+    </ur-table-column>
+    <ur-table-column label="操作">
+      <template #default="scope">
+        <ur-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</ur-button>
+        <ur-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</ur-button>
+      </template>
+    </ur-table-column>
+  </ur-table>
 </template>
 
 <script>
@@ -33,6 +80,14 @@ export default {
         },
       ],
     };
+  },
+  methods: {
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
+    },
   },
 };
 </script>
