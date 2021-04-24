59fb91a5ef3c29c8d0658229e2ffd264d2098ae1
Zimomo333 <lzh120908717@gmail.com>
2021-04-25 02:37:48 +0800

    import global style

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import App from './App.vue'
 // import UnrealUI from '@/lib/index'
 // import '@/lib/index/style.css'
 
-import UnrealUI from '@/packages/index'
+// import UnrealUI from '@/packages/index'
 
-// import UnrealUI from 'unreal-ui-next'
-// import 'unreal-ui-next/lib/index/style.css'
+import UnrealUI from 'unreal-ui-next'
+import 'unreal-ui-next/lib/ur-index/style.css'
 
 // import UnrealUI from 'unreal-ui-next'
 // import { UrButton } from 'unreal-ui-next'
