0a8b0bcd793a0720aae607a4d5dc9b9bbd1e2843
Zimomo333 <lzh120908717@gmail.com>
2021-04-29 22:29:43 +0800

    update：time-picker demo

--- src/components/time-picker.vue
+++ src/components/time-picker.vue
@@ -4,8 +4,8 @@
     :disabled-hours="disabledHours"
     :disabled-minutes="disabledMinutes"
     :disabled-seconds="disabledSeconds"
-    placeholder="任意时间点">
-  </ur-time-picker>
+    placeholder="任意时间点"
+  ></ur-time-picker>
   <ur-time-picker
     is-range
     arrow-control
@@ -19,12 +19,38 @@
 </template>
 
 <script>
+const makeRange = (start, end) => {
+  const result = [];
+  for (let i = start; i <= end; i++) {
+    result.push(i);
+  }
+  return result;
+};
 export default {
   data() {
     return {
       value1: new Date(2016, 9, 10, 8, 40),
       value2: [new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)],
     };
+  },
+  methods: {
+    // 如允许 17:30:00 - 18:30:00
+    disabledHours() {
+      return makeRange(0, 16).concat(makeRange(19, 23));
+    },
+    disabledMinutes(hour) {
+      if (hour === 17) {
+        return makeRange(0, 29);
+      }
+      if (hour === 18) {
+        return makeRange(31, 59);
+      }
+    },
+    disabledSeconds(hour, minute) {
+      if (hour === 18 && minute === 30) {
+        return makeRange(1, 59);
+      }
+    },
   },
 };
 </script>
